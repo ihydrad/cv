5f5a4530d6ec4c5af3e51b2013912e149c125100
ihydrad <86055286+ihydrad@users.noreply.github.com>
2023-02-23 00:03:33 +0500

    -

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-import argparse
 import cv2
 
-vcap = cv2.VideoCapture("rtsp://admin:admin@192.168.101.8:554/ch0_0.264", cv2.CAP_FFMPEG)
-ret, frame = vcap.read()
-cv2.imwrite("out.png", frame)
+# vcap = cv2.VideoCapture("rtsp://admin:admin@192.168.101.8:554/ch0_0.264")
+vcap = cv2.VideoCapture(0)
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+while True:
+    ret, frame = vcap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+vcap.release()
+cv2.destroyAllWindows()

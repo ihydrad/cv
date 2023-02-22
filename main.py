1d331c1f2920ddcced615a2f3b2b0badf19e4f67
ihydrad <86055286+ihydrad@users.noreply.github.com>
2023-02-22 21:43:23 +0500

    gitignore add

--- main.py
+++ main.py
@@ -3,6 +3,4 @@ import cv2
 
 vcap = cv2.VideoCapture("rtsp://admin:admin@192.168.101.8:554/ch0_0.264", cv2.CAP_FFMPEG)
 ret, frame = vcap.read()
-
-cv2.imshow('image', frame)
-cv2.waitKey(0)
+cv2.imwrite("out.png", frame)
